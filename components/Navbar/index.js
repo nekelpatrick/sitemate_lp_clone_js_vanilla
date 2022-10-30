5d8bc7a12b5825ab367f8ef50709268d6118544e
Patrick Nekel <nekelpatrick@gmail.com>
2022-10-30 17:09:02 -0300

    :lipstick: add main section

--- components/Navbar/index.js
+++ components/Navbar/index.js
@@ -3,37 +3,41 @@ import "./index.css";
 export const Navbar = () => {
   return `
   <nav class="navbar">
-     <div class="navbar-wrapper">
+  <div class="navbar-wrapper">
+  <ul class="navbar-links">
+  <img src="../../assets/img/sitemate1.svg" alt="" class="logo"/>
+     <li>
+       <a href="" class="bt">Products</a>
+     </li>
+     <li>
+       <a href="" class="bt">By system</a>
+     </li>
+     <li>
+       <a href="" class="bt">By use case</a>
+       <li>
+        <a href="" class="bt">Pricing</a>
+      </li>
+ 
+     </li>
+     <li>
+       <a href="" class="bt">Resources</a>
+     </li>
+     </ul>
+     
      <ul class="navbar-links">
-     <img src="../../assets/img/sitemate1.svg" alt="" class="logo"/>
-        <li>
-          <a href="" class="bt">Products</a>
-        </li>
-        <li>
-          <a href="" class="bt">By system</a>
-        </li>
-        <li>
-          <a href="" class="bt">By use case</a>
-        </li>
-        <li>
-          <a href="" class="bt">Resources</a>
-        </li>
-        </ul>
-        
-        <ul class="navbar-links">
-        
-        <li>
-          <a href="" class="bt">Login</a>
-        </li>
-        <li>
-        <a href="" class="bt bt-secondary">Watch demo</a>
-        </li>
-        <li>
-        <a href="" class="bt bt-primary">Try it for free</a>
-        </li>
-        
-      </ul>
-  </div>
+     
+     <li>
+       <a href="" class="bt">Login</a>
+     </li>
+     <li>
+     <a href="" class="bt bt-secondary">Watch demo</a>
+     </li>
+     <li>
+     <a href="" class="bt bt-primary">Try it for free</a>
+     </li>
+     
+   </ul>
+</div>
 </nav>
     
     `;
--- pages/Home/index.js
+++ pages/Home/index.js
@@ -5,14 +5,14 @@ export const Home = () => {
   <main>
   <section class="main-section">
     <div class="action">
-      <h1>You weren't born to do paperwork</h1>
+      <h2>You weren't born to do paperwork</h2>
       <p>
         Easily build or convert your ideal processes into a single touch digital
         format.
       </p>
       <div>
-        <button class="bt bt-primary">Try the software free</button>
-        <button class="bt bt-secondary">Watch demo</button>
+        <button class="bt bt-primary bt-home">Try the software free</button>
+        <button class="bt bt-secondary bt-home bt-watch">Watch demo</button>
       </div>
     </div>
     <div class="main-img">
@@ -22,7 +22,7 @@ export const Home = () => {
   <section class="partners-section">
     <img src="../../assets/img/sitemate5.svg" alt="" />
     <div class="partners">
-      Trusted by companies, projects and teams of all sizes
+      <p>Trusted by companies, projects and teams of all sizes </p>
     </div>
   </section>
 </main>
